e365eddd59d4ef9621e54a64514e4e89ef2ef51a
Felipe Hernández Palafox <felipe.sistemas.fhp@gmail.com>
2021-01-01 14:04:10 -0600

    cambios pagina web blog
    cambios pagina web blog

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
 	
-	/* ======= Highlight.js Plugin ======= */ 
-    /* Ref: https://highlightjs.org/usage/ */     
-    $('pre code').each(function(i, block) {
-	    hljs.highlightBlock(block);
-	 });
-
+$('.navbar-nav li').on('click', function(){
+	$('ul.navbar-nav li').each(function (i) {
+		$('ul.navbar-nav li.active').removeClass('active');
+	});
+	$(this).addClass('active');
+});
+	 
 });
